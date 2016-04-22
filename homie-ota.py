fbcbcce723b1ae300cda04d3850b8cfb5f3e34e8
Ben Jones <ben.jones12@gmail.com>
2016-04-23 01:10:32 +1200

    moved all configurable params to top - in preparation for INI file support

--- homie-ota.py
+++ homie-ota.py
@@ -6,8 +6,15 @@ from bottle import get, request, run, static_file, HTTPResponse
 import StringIO
 import os
 
-#firmware_root = '/mnt/owncloud/ben/files/data/firmware'
+
+# configuration
+# TODO: move to ini file
+host = '0.0.0.0'
+port = 9080
+verbose = True
+endpoint = '/ota'
 firmware_root = '.'
+
 
 # X-Esp8266-Ap-Mac = 1A:FE:34:CF:3A:07
 # X-Esp8266-Sta-Mac = 18:FE:34:CF:3A:07
@@ -23,22 +30,22 @@ firmware_root = '.'
 # X-Esp8266-Version = cf3a07e0=h-sensor=1.0.1=1.0.2
 # Content-Type = text/plain
 
-@get('/ota')
+@get(endpoint)
 def ota():
 
     headers = request.headers
     for k in headers:
-        print k, '=', headers[k]
+        debug(k + ' = ' + headers[k])
 
     # TODO: check free space vs .bin file on disk and refuse
 
     try:
         device, firmware_name, have_version, want_version = headers.get('X-Esp8266-Version', None).split('=')
     except:
-        print "Can't find X-Esp8266-Version in headers"
+        debug("Can't find X-Esp8266-Version in headers")
         return HTTPResponse(status=403, body="Not permitted")
 
-    print "Homie firmware=%s, have=%s, want=%s on device=%s" % (firmware_name, have_version, want_version, device)
+    debug("Homie firmware=%s, have=%s, want=%s on device=%s" % (firmware_name, have_version, want_version, device))
 
     # <firmware_root>/<firmware_name>/<firmware_name-x.x.x.bin
     # e.g. './h-sensor/h-sensor-1.0.3.bin'
@@ -47,10 +54,14 @@ def ota():
     binary_path = "%s/%s" % (firmware_path, binary)
 
     if os.path.exists(binary_path):
-        print "Return OTA firmware %s" % (binary_path)
+        debug("Return OTA firmware %s" % (binary_path))
         return static_file(binary, root=firmware_path)
 
-    print "%s not found; returning 403" % binary_path
+    debug("%s not found; returning 403" % (binary_path))
     return HTTPResponse(status=403, body="Firmware not found")
 
-run(host='0.0.0.0', port=9080, debug=True)
+def debug(message):
+    if verbose:
+        print message
+
+run(host=host, port=port, debug=debug)
